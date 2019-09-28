f85f3bef785fbe323e25aafbbebc0b50573440a3
'eyoelyil' <'eyoelyil22@gmail.com'>
2019-09-29 02:32:34 +0300

    First commit

---
+++ EmailApp/src/EmailGeneratingApp/Email.java
@@ -0,0 +1,91 @@
+package EmailGeneratingApp;
+
+import java.util.Scanner;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String department;
+	private String alternateEmail;
+	private int mailboxCapacity = 500;
+	private String companyName = "eyocompany.com";
+	private int defaultPasswordLength = 10;
+
+	// Constructor to receive firstName and lastName
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+		// Call a method asking for the department and then return the department
+		this.department = setDepartment();
+
+		// Call a method that return a random password
+		this.password = randomPassword(defaultPasswordLength);
+
+		// Combine elements to generate email
+		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companyName;
+	}
+
+
+	// Ask for the department
+	private String setDepartment() {
+		System.out.print(
+				"New worker: " + firstName
+						+ ". Department Codes\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) {
+			return "sales";
+		} else if (depChoice == 2) {
+			return "developement";
+		} else if (depChoice == 3) {
+			return "accounting";
+		} else {
+			return "";
+		}
+	}
+
+	// Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#%&@";
+		char[] password = new char[length];
+		for (int i = 0; i < length; i++) {
+			int random = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(random);
+		}
+		return new String(password);
+	}
+	// Set the mailbox capacity
+	public void setMailboxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	}
+
+	// Set the alternate email
+	public void setAlternateEmail(String altEmail) {
+		this.alternateEmail = altEmail;
+	}
+
+	// Change the password
+	public void changePassword(String password) {
+		this.password = password;
+	}
+
+	public int getMailboxCapacity() {
+		return mailboxCapacity;
+	}
+
+	public String getAlternateEmail() {
+		return alternateEmail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String showInfo() {
+		return "DISPLAY NAME: " + firstName + " " + lastName + "\nCOMPANY EMAIL: " + email + "\nMAILBOX CAPACITY: "
+				+ mailboxCapacity + "mb";
+	}
+}
---
+++ EmailApp/src/EmailGeneratingApp/EmailRunner.java
@@ -0,0 +1,9 @@
+package EmailGeneratingApp;
+
+public class EmailRunner {
+
+	public static void main(String[] args) {
+		Email email1 = new Email("Eyoel", "Mul");
+		System.out.println(email1.showInfo());
+	}
+}
